000806416f0c9657d571277cbda2ef8cbfd70b62
Eric Rohm <rohm.physics@gmail.com>
2019-08-13 11:57:56 -0400

    Added plotting function

--- coordinate_mapping.py
+++ coordinate_mapping.py
@@ -14,37 +14,17 @@ array = tree2rec(intree)
 
 fig = pyplot.figure()
 ax = Axes3D(fig)
-'''
-f=open("ChannelPosition.txt", "r")
 
-list=[]
-f1 = f.readlines()
-for x in f1:
-	list.append(x)
-print(list[5])
-print(list[6])
-print(list[6][0])
- list[6][0] returns "7", the crystal number. Need to pay attention not to get an off by one error 
-print(list[0])
-print(list[0][5])
+coordinates = np.loadtxt('ChannelPosition.dat')
+#print(coordinates[0][0])
 
-list = [[] for j in range(0,987)]
-
-with open('ChannelPosition.txt') as f:
-    data = f.read().splitlines()
-    data = [i.split() for i in data if any(j.isdigit() for j in i)]
-    for i in data:
-        print('Crystal: {}\nx: {}\ny: {}\nz: {}'.format(i[0], i[1], i[2], i[3]))
-        print("==================================")
-        list[i].extend([i[1], i[2], i[3]])
-        ax.scatter(xs=i[1], ys=i[2], zs=i[3])
-print(list)
+plt.figure()
+for i in range (0,988):
+	print(coordinates[i][0],": ", coordinates[i][1], ", ", coordinates[i][2], ", ", coordinates[i][3])
+	ax.scatter(xs=coordinates[i][1], ys=coordinates[i][2], zs=coordinates[i][3], color='b')
 plt.show()
-'''
-data = np.loadtxt('ChannelPosition.dat')
-#print(data[0][0])
-print(data[0][0])
-''' NEXT: create a dictionary (or even a list) with these values, the keys being crystal number, holding x, y, z values. Then figure out how to plot the coordinates. ''' 
+	
+''' The ith crystal number coordinates are contained in the (i+1)th element of the list '''
 
 
 
